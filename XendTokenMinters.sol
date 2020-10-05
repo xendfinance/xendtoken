e0e127a68d12bedaf8d07c7d372bb56591f20641
Chukwuemeka Nweke <emekanweke604@gmail.com>
2020-10-05 22:28:47 +0100

    Added token contracts

---
+++ XendTokenMinters.sol
@@ -0,0 +1,29 @@
+pragma solidity ^0.6.4;
+import "./Ownable.sol";
+
+contract XendTokenMinters is Ownable {
+    mapping(address => bool) public minters;
+
+    modifier onlyMinter() {
+        bool hasAccess = minters[msg.sender];
+        require(
+            hasAccess == true,
+            "mint access has not been granted to this account"
+        );
+        _;
+    }
+
+    function grantAccess(address minter) public onlyOwner {
+        bool hasAccess = minters[minter];
+
+        require(hasAccess == false, "minter has already been granted access");
+        minters[minter] = true;
+    }
+
+    function revokeAccess(address minter) public onlyOwner {
+        bool hasAccess = minters[minter];
+
+        require(hasAccess == true, "minter has not been granted access");
+        minters[minter] = false;
+    }
+}
